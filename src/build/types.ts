14edcedd2d6965b9cd3d7fa8e14e0766fa499ede
feiYu <feiyucode@gmail.com>
2025-08-16 23:33:15 +0800

    fix: 修复类型必填错误

--- src/build/types.ts
+++ src/build/types.ts
@@ -9,13 +9,13 @@ export type DefaultOptions = {
 	/** 输出目录, 默认值为 './dist' */
 	outDir?: TsdownOptions['outDir']
 	/** 输出格式, 默认值为 'esm' */
-	format: TsdownOptions['format']
+	format?: TsdownOptions['format']
 	/** 运行平台, 默认值为 'node' */
-	platform: TsdownOptions['platform']
+	platform?: TsdownOptions['platform']
 	/** 不拆包, 默认为 true */
-	unbundle: TsdownOptions['unbundle']
+	unbundle?: TsdownOptions['unbundle']
 	/** node 内置模块添加 node: 前缀, 默认为 true */
-	nodeProtocol: TsdownOptions['nodeProtocol']
+	nodeProtocol?: TsdownOptions['nodeProtocol']
 }
 
 export type Ctx = {
